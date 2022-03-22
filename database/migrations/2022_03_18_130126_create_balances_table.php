983bfe528bcca8a7e8c411d56eb1d31d52e883dc
Unknown2805 <nuraliali2805@gmail.com>
2022-03-22 10:30:07 +0700

    Fixing Bugs on Delete User & Inventory

--- database/migrations/2022_03_18_130126_create_balances_table.php
+++ database/migrations/2022_03_18_130126_create_balances_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('balances', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->integer('balance');
             $table->timestamps();
         });
--- database/migrations/2022_03_18_130135_create_transactions_table.php
+++ database/migrations/2022_03_18_130135_create_transactions_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
-            $table->foreignId('inventory_id')->nullable()->constrained();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('inventory_id')->nullable()->constrained()->onDelete('cascade');
             $table->integer('amount');
             $table->string('invoice_id')->nullable();
             $table->integer('type');
